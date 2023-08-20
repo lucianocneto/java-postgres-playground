9ed35989e8a837ba91094e5d5b04ddf54fa2837f
Luciano Neto <lucianocneto@gmail.com>
2023-08-20 13:47:11 +0000

    Add Comentarios

--- src/main/java/com/example/AppLNeto.java
+++ src/main/java/com/example/AppLNeto.java
@@ -6,7 +6,8 @@ public class AppLNeto {
 
         //byte idade = 128;
         int aniversario = 127;
-        byte idade = (byte)aniversario;
+        byte idade = (byte)aniversario; //conversao de inteiro pra byte
+        //byte aceita de 1 a 127
 
         double peso = 88.5;
         System.out.println("Idade é: "+idade+" Peso é: "+peso);
