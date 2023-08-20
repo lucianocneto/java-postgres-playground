acdabdd43f10579bf3f4069bf6c3624aab08da88
Luciano Neto <lucianocneto@gmail.com>
2023-08-20 13:45:38 +0000

    Add Exercicio Idade e Peso

--- src/main/java/com/example/AppLNeto.java
+++ src/main/java/com/example/AppLNeto.java
@@ -2,6 +2,13 @@ package com.example;
 
 public class AppLNeto {
     public static void main(String[] args) {
-        System.out.println("OLA MUNDO");
+        System.out.println("Minha App Java\n");
+
+        //byte idade = 128;
+        int aniversario = 127;
+        byte idade = (byte)aniversario;
+
+        double peso = 88.5;
+        System.out.println("Idade é: "+idade+" Peso é: "+peso);
     }
 }
