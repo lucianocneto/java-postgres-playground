78a0b12a6c9bc70befbcd8927c8ea440a20ecece
Luciano Neto <lucianocneto@gmail.com>
2023-08-22 00:47:04 +0000

    testando classes

---
+++ src/main/java/com/example/AppClasses.java
@@ -0,0 +1,11 @@
+package com.example;
+
+class Cliente {
+    int x;
+}
+
+public class AppClasses {
+    public static void main(String[] args) {
+        System.out.println("Criando Classes Java");
+    }
+}
