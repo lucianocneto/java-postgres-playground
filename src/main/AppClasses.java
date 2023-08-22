51fd6afa3fa004c1562cd3bc891e6367074e112f
Luciano Neto <lucianocneto@gmail.com>
2023-08-22 00:41:26 +0000

    ALTERAÇÕES CLASSES

--- src/main/AppClasses.java
+++ src/main/AppClasses.java
@@ -1,3 +1,18 @@
+package com.example;
+
+class Cliente {
+    double renda;
+    char sexo;
+    int anoNascimento;
+}
+
 public class AppClasses {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Criando classes");
+        Cliente cliente = new Cliente();
+        System.out.println("Renda: " + cliente.renda);
+        System.out.println("Sexo: " + cliente.sexo);
+        System.out.println("Ano de Nascimento: " + cliente.anoNascimento);
+    }
 }
